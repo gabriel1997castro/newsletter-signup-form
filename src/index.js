7070ea1d44ed1b7498fb8bf0161f5c062f20a824
Gabriel <gabriel1997.castro@gmail.com>
2022-06-17 10:46:09 -0300

    Spread props instead of passing directly

--- src/index.js
+++ src/index.js
@@ -32,33 +32,24 @@ const SignupForm = () => {
       <label htmlFor="firstName">First Name</label>
       <input
         id="firstName"
-        name="firstName"
         type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.firstName}
+        {...formik.getFieldProps('firstName')}
       />
       {formik.touched.firstName && formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
 
       <label htmlFor="lastName">Last Name</label>
       <input
         id="lastName"
-        name="lastName"
         type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.lastName}
+        {...formik.getFieldProps('firstName')}
       />
       {formik.touched.lastName && formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
 
       <label htmlFor="email">Email Address</label>
       <input
         id="email"
-        name="email"
         type="email"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
+        {...formik.getFieldProps('firstName')}
       />
       {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
 
