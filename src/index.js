edf2a94e3928376c4ee81e2385a78fd09d4085ab
Gabriel <gabriel1997.castro@gmail.com>
2022-06-17 13:15:16 -0300

    The less verbose way of using Formik

--- src/index.js
+++ src/index.js
@@ -1,60 +1,63 @@
-import React from "react";
 import { createRoot } from 'react-dom/client';
-import { useFormik } from "formik";
-import * as Yup from 'yup';
 import "./styles.css";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from 'yup';
 
 const SignupForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      firstName: "",
-      lastName: "",
-      email: ""
-    },
-    validationSchema: Yup.object({
-      firstName: Yup.string()
-        .max(15, "Must be 15 characters or less")
-        .required("Required"),
-      lastName: Yup.string()
-        .max(20, "Must be 20 characters or less")
-        .required("Required"),
-      email: Yup.string()
-        .email("Invalid email address")
-        .required("Required")
-    }),
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    }
-  });
-
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="firstName">First Name</label>
-      <input
-        id="firstName"
-        type="text"
-        {...formik.getFieldProps('firstName')}
-      />
-      {formik.touched.firstName && formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+        comments: ""
+      }}
+      validationSchema={Yup.object({
+        firstName: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
+        lastName: Yup.string()
+          .max(20, "Must be 20 characters or less")
+          .required("Required"),
+        email: Yup.string()
+          .email("Invalid email address")
+          .required("Required"),
+        comments: Yup.string()
+          .max(200, "Must be 200 characters or less")
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field name="firstName" type="text" className="form-input" />
+        <ErrorMessage name="firstName" />
 
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        id="lastName"
-        type="text"
-        {...formik.getFieldProps('lastName')}
-      />
-      {formik.touched.lastName && formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+        <label htmlFor="lastName">Last Name</label>
+        <Field name="lastName" type="text" />
+        <ErrorMessage name="lastName" />
 
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        type="email"
-        {...formik.getFieldProps('email')}
-      />
-      {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
+        <label htmlFor="email">Email Address</label>
+        <Field name="email" type="email" />
+        <ErrorMessage name="email" />
 
-      <button type="submit">Submit</button>
-    </form>
+        <label htmlFor="comments">Comments</label>
+        <Field name="comments" as="textarea" className="form-textarea" />
+        <ErrorMessage name="comments" />
+
+        <label htmlFor="notifications">Do you accept to receive email notifications?</label>
+        <Field name="notifications" as="select" className="select-notifications">
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+        </Field>
+        <br />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik >
   );
 };
 
