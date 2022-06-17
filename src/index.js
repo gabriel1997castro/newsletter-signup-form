b0661f92ef3405af5a18c0c8821c8261abdd54bb
Gabriel <gabriel1997.castro@gmail.com>
2022-06-17 15:54:12 -0300

    Separate components

--- src/index.js
+++ src/index.js
@@ -1,56 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import "./styles.css";
 import React from 'react';
-import { Formik, Form, useField } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
+import { TextInput } from './components/TextInput';
+import { Checkbox } from './components/Checkbox';
+import { Select } from './components/Select';
 
-const MyTextInput = ({ label, ...props }) => {
-  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-  // which we can spread on <input>. We can use field meta to show an error
-  // message if the field is invalid and it has been touched (i.e. visited)
-  const [field, meta] = useField(props);
-  return (
-    <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="text-input" {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
-};
 
-const MyCheckbox = ({ children, ...props }) => {
-  // React treats radios and checkbox inputs differently other input types, select, and textarea.
-  // Formik does this too! When you specify `type` to useField(), it will
-  // return the correct bag of props for you -- a `checked` prop will be included
-  // in `field` alongside `name`, `value`, `onChange`, and `onBlur`
-  const [field, meta] = useField({ ...props, type: 'checkbox' });
-  return (
-    <div>
-      <label className="checkbox-input">
-        <input type="checkbox" {...field} {...props} />
-        {children}
-      </label>
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </div>
-  );
-};
 
-const MySelect = ({ label, ...props }) => {
-  const [field, meta] = useField(props);
-  return (
-    <div>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <select {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </div>
-  );
-};
 
 // And now we can use these
 const SignupForm = () => {
@@ -95,38 +53,38 @@ const SignupForm = () => {
       >
         {formik => (
           <Form>
-            <MyTextInput
+            <TextInput
               label="First Name"
               name="firstName"
               type="text"
               placeholder="Jane"
             />
 
-            <MyTextInput
+            <TextInput
               label="Last Name"
               name="lastName"
               type="text"
               placeholder="Doe"
             />
 
-            <MyTextInput
+            <TextInput
               label="Email Address"
               name="email"
               type="email"
               placeholder="jane@formik.com"
             />
 
-            <MySelect label="Job Type" name="jobType">
+            <Select label="Job Type" name="jobType">
               <option value="">Select a job type</option>
               <option value="designer">Designer</option>
               <option value="development">Developer</option>
               <option value="product">Product Manager</option>
               <option value="other">Other</option>
-            </MySelect>
+            </Select>
 
-            <MyCheckbox name="acceptedTerms">
+            <Checkbox name="acceptedTerms">
               I accept the terms and conditions
-            </MyCheckbox>
+            </Checkbox>
 
             <button disabled={!formik.isValid || formik.isSubmitting} type="submit">Submit</button>
             <button type="reset">Clear form</button>
